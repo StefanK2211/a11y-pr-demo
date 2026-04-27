77d6f0c26b1ffb48c6fb377f6e683a5c236a7056
Stefan Kingham <stefankingham@MacBook-Pro.local>
2026-04-28 02:17:13 +0300

    Fix server startup for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,14 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run dev -- --host',
     url: 'http://127.0.0.1:5173',
-    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+    reuseExistingServer: false,
+  },
+
+  use: {
+    baseURL: 'http://127.0.0.1:5173',
   },
 
   projects: [
